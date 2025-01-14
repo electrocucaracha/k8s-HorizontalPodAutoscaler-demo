91e21b1c56e6fc84d5b4f9636a9e50d64250faa0
Victor Morales <chipahuac@hotmail.com>
2025-01-13 22:04:37 -0800

    Drop dependabot

--- ci/update_versions.sh
+++ ci/update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c)
+# Copyright (c) 2025
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
@@ -13,6 +13,8 @@ set -o errexit
 set -o nounset
 
 last_version=$(curl -sL https://registry.hub.docker.com/v2/repositories/kindest/node/tags | python -c 'import json,sys,re;versions=[obj["name"][1:] for obj in json.load(sys.stdin)["results"] if re.match("^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$",obj["name"])];print("\n".join(versions))' | uniq | sort -rn | head -n 1)
+
+trap "make fmt" EXIT
 
 cat <<EOT >scripts/kind-config.yml
 ---
@@ -56,3 +58,11 @@ if command -v go >/dev/null; then
     fi
     "$GOPATH/bin/cyclonedx-gomod" mod -licenses -json -output mod_k8s-HorizontalPodAutoscaler-demo.bom.json
 fi
+
+# Update GitHub Action commit hashes
+gh_actions=$(grep -r "uses: [a-zA-Z\-]*/[\_a-z\-]*@" .github/ | sed 's/@.*//' | awk -F ': ' '{ print $3 }' | sort -u)
+for action in $gh_actions; do
+    commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/[vV]\?[\.]\?||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+    # shellcheck disable=SC2267
+    grep -ElRZ "uses: $action@" .github/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
+done
